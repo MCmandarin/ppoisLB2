f19f6a6d588d3241f6fe64d09f0cc543fd137cf4
daniil <daniilpototskiy@mail.ru>
2023-11-16 09:28:58 +0300

    Changes exception

--- src/ppois/lb2/ConstructionSite.java
+++ src/ppois/lb2/ConstructionSite.java
@@ -37,7 +37,7 @@ public class ConstructionSite {
         this.workEndTime = workEndTime;
     }
 
-    @OverrideSyste
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
